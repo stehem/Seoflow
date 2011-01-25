13fe6c39eb78cc3bcff1c2d6b314c83cc768a3a8
stephane <stephane@stephane-desktop.(none)>
2011-01-25 21:31:14 +0100

    seoflow

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 class TagsController < ApplicationController
 respond_to :html , :js
-
+ 
 
   def index
-    @tags = Tag.count(:group => :tag, :order => 'count(tag) DESC').to_a.paginate(:per_page => 40, :page => params[:page])
+    @tags = Tag.count(:group => :tag).to_a.paginate(:per_page => 40, :page => params[:page])
   end
 
 end
