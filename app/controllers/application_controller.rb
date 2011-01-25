b01206663cf7457ac74ea610b9064203f8395301
stephane <stephane@stephane-desktop.(none)>
2011-01-25 22:03:25 +0100

    seoflow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   require 'sanitize'
-  before_filter :current_user, :check_uri
+  before_filter :current_user, :set_cache_buster, :check_uri
 
   def current_user
     if session[:login]
@@ -15,6 +15,13 @@ class ApplicationController < ActionController::Base
 
   def check_uri
     redirect_to(request.protocol + "www." + request.host_with_port + request.request_uri , :status => :moved_permanently) if !/^www/.match(request.host)
+  end
+
+  
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
   
 end
