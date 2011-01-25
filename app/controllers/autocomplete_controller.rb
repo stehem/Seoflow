a21e793b8656fcdafdf09c4cd13374abc55752f8
stephane <stephane@stephane-desktop.(none)>
2011-01-25 21:51:12 +0100

    seoflow

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -16,9 +16,9 @@ end
 def tags_search
 query = "%#{params[:filter]}%"
 if params[:filter].blank?
-    @tags = Tag.count(:group => :tag, :order => :tag).to_a.paginate(:per_page => 40, :page => params[:page])
+    @tags = Tag.count(:group => :tag, :order => 'count(tag) DESC').to_a.paginate(:per_page => 40, :page => params[:page])
 else
-  @tags = Tag.count(:group => :tag, :order => :tag, :conditions => ["tag LIKE ?", query]).to_a.paginate(:per_page => 40, :page => params[:page])
+  @tags = Tag.count(:group => :tag, :order => 'count(tag) DESC', :conditions => ["tag LIKE ?", query]).to_a.paginate(:per_page => 40, :page => params[:page])
 end
 end
 
